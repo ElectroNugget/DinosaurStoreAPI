cbe34b134652a65a1125272443c8898ffcf5e620
John Henrik Muller <johnhenrikmuller@gmail.com>
2021-04-25 22:32:43 +0200

    It's working!

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-import express from 'express';
+import express from "express";
+
+import { customersRouter } from "./customers/customers.route.js";
+import { cartsRouter } from "./carts/carts.route.js";
+import { productsRouter } from "./products/products.route.js";
 
 const app = express();
-const PORT = 3000; 
-app.get('/', (req, res) => res.send('Server:Hello World!'));
+const PORT = 8000;
 
-app.listen(PORT, function(err){ 
-    if (err) console.log("Error in server setup") 
-    console.log("Server! listening on Port", PORT); 
-});
+app.use(express.json());
+
+//Routers
+app.use(customersRouter);
+app.use(cartsRouter);
+app.use(productsRouter);
+
+// From the tutorial.
+app.get("/", (req, res) => res.send("Welcome to localhost 8000!"));
+app.listen(PORT, () => console.log("DinoAPI listening on port 8000!"));
